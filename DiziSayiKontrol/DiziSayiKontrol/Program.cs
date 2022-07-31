7bb4015b172e6fd1eeb59bb959da5855e88fd18f
fatihbereketinan <fatihbereketinan@gmail.com>
2022-07-31 19:51:40 +0300

    CSharp_Arrays_Diziler

---
+++ DiziSayiKontrol/DiziSayiKontrol/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DiziSayiKontrol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = new int[] { 5, 10, 15, 35, 45, 8, 3, 12, 100, 85, 75, 62, 69, 83, 71, 71, 85, 71, 5, 10, 71 };
+
+            while (true)
+            {
+                int toplam = 0;
+
+                Console.Write("Lütfen bir sayi giriniz: ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < sayilar.Length; i++)
+                {
+                    if (sayilar[i] == sayi)
+                    {
+                        toplam = toplam + 1;
+                    }
+                }
+                if (toplam > 0)
+                {
+                    Console.WriteLine(sayi + " sayısı dizi içerisinde " + toplam + " adet bulunmuştur");
+                }
+                else
+                {
+                    Console.WriteLine("Girdiğiniz sayı dizi içerisinde bulunamamıştır.");
+                }
+            }
+        }
+    }
+}
